c509806e8d7c73ea7d487a7de1d8b173358aee40
Andrew Tarnowski <liimbix@gmail.com>
2016-01-25 18:15:45 -0500

    Create OatmealCookie.js

---
+++ OatmealCookie.js
@@ -0,0 +1,24 @@
+function BakeCookie(name,value) {
+   var oDate = new Date();
+    oDate.setYear(oDate.getFullYear()+1);
+    var oCookie = encodeURIComponent(name) + '=' + encodeURIComponent(value) + ';expires=' + oDate.toGMTString() + ';path=/';
+    document.cookie= oCookie; 
+}
+
+function EatCookie(name){
+        name = name.toLowerCase();
+    var oCrumbles = document.cookie.split(';');
+    for(var i=0; i<oCrumbles.length;i++)
+    {
+        var oPair= oCrumbles[i].split('=');
+        var oKey = decodeURIComponent(oPair[0].trim().toLowerCase());
+        var oValue = oPair.length>1?oPair[1]:'';
+        if(oKey == name)
+            return decodeURIComponent(oValue);
+    }
+    return '';
+}
+
+function BurnCookie(name){
+    BakeCookie(name,'');
+}
